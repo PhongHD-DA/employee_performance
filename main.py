77934cf0dbd4955357d9147c49eb23b1fbaef562
PhongHD-DA <contact.phonghd@gmail.com>
2026-05-14 12:10:02 +0700

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from staging_pipeline import build_staging_layer
-from analytics_pipeline import build_analytics_layer
-
-if __name__ == "__main__":
-    build_staging_layer()
-    build_analytics_layer()
